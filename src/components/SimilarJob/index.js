728b2cef53343c9d58a0226607f1eabf82bea3c3
saisuchal <suchal.sadula528@gmail.com>
2025-03-18 15:22:53 +0530

    Update index.js

--- src/components/SimilarJob/index.js
+++ src/components/SimilarJob/index.js
@@ -6,36 +6,33 @@ import './index.css'
 const SimilarJob = props => {
   const {job} = props
   return (
-    <li>
-      <div className="similar-div">
-        <div className="similar-flex-title">
-          <img
-            className="company-logo"
-            src={job.companyLogoUrl}
-            alt="similar job company logo"
-          />
-          <div className="title">
-            <h2 className="heading">{job.title}</h2>
-            <div className="flex-title">
-              <FaStar className="icon" />
-              <p>{job.rating}</p>
-            </div>
-          </div>
-        </div>
+    <li className="similar-job">
+      <div className="similar-flex-title">
+        <img
+          className="similar-company-logo"
+          src={job.companyLogoUrl}
+          alt="similar job company logo"
+        />
         <div>
-          <h1>Description</h1>
-          <p className="para-line">{job.jobDescription}</p>
+          <h1 className="title-head">{job.title}</h1>
+          <p>
+            <FaStar className="icon" />
+            {job.rating}
+          </p>
         </div>
-        <div className="flex-details">
-          <div className="details">
-            <p className="icon">
-              <MdLocationOn /> {job.location}
-            </p>
-            <p className="icon">
-              <BsBagFill />
-              {job.employmentType}
-            </p>
-          </div>
+      </div>
+      <div className="show-description">
+        <h1 className="description-head">Description</h1>
+        <p className="para-line">{job.jobDescription}</p>
+      </div>
+      <div className="similar-flex-details-div">
+        <div className="similar-details">
+          <p className="similar-icon">
+            <MdLocationOn /> {job.location}
+          </p>
+          <p className="similar-icon">
+            <BsBagFill /> {job.employmentType}
+          </p>
         </div>
       </div>
     </li>
